b5f9198c78c44ff4e3bedd0220ef23d4971ed255
Sajidhussain123 <sajidmcsbwn@gmail.com>
2024-04-30 22:06:19 +0500

    login signup completed

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,18 +1,28 @@
 import mongoose from "mongoose";
 
-export function connectToDB() {
+async function connectToDB() {
   try {
-    mongoose.connect(process.env.MONGO_URI!);
-    const connection = mongoose.connection;
-    connection.on("connected", () => {
-      console.log("connected to mongoDB successfully");
-    });
-    connection.on("error", (error) => {
-      console.log("error connecting to mongoDB", error);
-    });
-    process.exit();
-  } catch (error) {
-    console.error("some thing went wrong");
-    console.log(error);
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("Connected to MongoDB");
+  } catch (error: any) {
+    console.error("Error connecting to MongoDB:", error.message);
   }
+
+  // try {
+  //   mongoose.connect(process.env.MONGO_URI!);
+  //   const connection = mongoose.connection;
+  //   console.log("1", connection);
+  //   connection.on("connected", () => {
+  //     console.log("connected to mongoDB successfully");
+  //   });
+  //   connection.on("error", (error) => {
+  //     console.log("error connecting to mongoDB", error);
+  //   });
+  //   process.exit();
+  // } catch (error) {
+  //   console.error("some thing went wrong");
+  //   console.log(error);
+  // }
 }
+
+export default connectToDB;
