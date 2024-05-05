9cf513878b60b4f7e74910e2525a622bef74d930
Sajid Hussain <sajidmcsbwn@gmail.com>
2024-05-05 11:59:50 +0500

    logout functionality added

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  try {
+    //creating response
+    const response = NextResponse.json({
+      message: "Logout successfully",
+      success: true,
+    });
+    // set cookies empty on logout
+    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+    // return response
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
