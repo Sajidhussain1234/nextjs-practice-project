a329e6488ce79cbfae6ff3736ba75b2bd3104aa0
Sajid Hussain <sajidmcsbwn@gmail.com>
2024-05-24 22:46:53 +0500

    profile - [id] update

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,3 +1,7 @@
 export default function UserProfile({ params }: any) {
-  return <div>This profile is of {params.id} user</div>;
+  return (
+    <div>
+      Info: Data getting from url -- This profile is of {params.id} user
+    </div>
+  );
 }
